74cf620f7ca29ec8218f27dab51ece48e456bbad
Yigithan Guven <demirejderha@gmail.com>
2023-01-19 13:31:41 +0300

    feat: Added Lobby panel

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -6,15 +6,20 @@ public class LaunchManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject EnterGamePanel;
     [SerializeField] private GameObject ConnectionStatusPanel;
+    [SerializeField] private GameObject LobbyPanel;
 
     private void Start()
     {
         EnterGamePanel.SetActive(true);
         ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
     }
     public override void OnConnectedToMaster()
     {
+        ConnectionStatusPanel.SetActive(false);
         Debug.Log(PhotonNetwork.NickName+" Connected to Photon Server");
+        LobbyPanel.SetActive(true);
+        
     }
     public void ConnectToPhotonServer()
     {
