96ebdbfb70874f2a80d5fc8350333dec1543113c
Yigithan Guven <demirejderha@gmail.com>
2023-01-19 11:12:01 +0300

    feat: Added connecting screen to loby

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using Photon.Pun;
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private GameObject EnterGamePanel;
+    [SerializeField] private GameObject ConnectionStatusPanel;
+
+    private void Start()
+    {
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+    }
     public override void OnConnectedToMaster()
     {
         Debug.Log(PhotonNetwork.NickName+" Connected to Photon Server");
@@ -11,6 +19,12 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     public void ConnectToPhotonServer()
     {
         if (!PhotonNetwork.IsConnected)
+        {
             PhotonNetwork.ConnectUsingSettings();
+            ConnectionStatusPanel.SetActive(true);
+            EnterGamePanel.SetActive(false);
+        }
+            
+
     }
 }
