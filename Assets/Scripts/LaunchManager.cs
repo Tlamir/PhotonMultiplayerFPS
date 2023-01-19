aca70f0d3a860eaf709481d5bc1f05140e172baa
Yigithan Guven <demirejderha@gmail.com>
2023-01-19 14:04:39 +0300

    feat: Added join create room

--- Assets/Scripts/LaunchManager.cs
+++ Assets/Scripts/LaunchManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System;
+using Photon.Realtime;
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject EnterGamePanel;
@@ -14,13 +16,39 @@ public class LaunchManager : MonoBehaviourPunCallbacks
         ConnectionStatusPanel.SetActive(false);
         LobbyPanel.SetActive(false);
     }
+    #region PhotonCallbacks
     public override void OnConnectedToMaster()
     {
         ConnectionStatusPanel.SetActive(false);
         Debug.Log(PhotonNetwork.NickName+" Connected to Photon Server");
         LobbyPanel.SetActive(true);
-        
     }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName + " joined to room : " + PhotonNetwork.CurrentRoom.Name);
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to room:  " + PhotonNetwork.CurrentRoom.Name);
+    }
+    #endregion
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room " + UnityEngine.Random.Range(0, 1000);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsOpen = true;
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 20;
+
+        PhotonNetwork.CreateRoom(randomRoomName,roomOptions);
+    }
+
     public void ConnectToPhotonServer()
     {
         if (!PhotonNetwork.IsConnected)
@@ -29,7 +57,9 @@ public class LaunchManager : MonoBehaviourPunCallbacks
             ConnectionStatusPanel.SetActive(true);
             EnterGamePanel.SetActive(false);
         }
-            
-
+    }
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
     }
 }
